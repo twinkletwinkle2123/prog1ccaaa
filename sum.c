ad9e05ee2b5448695c07b5e1fbc83e5e4e6483fd
cca class <littlestar3138@gmail.com>
2026-04-02 13:54:05 +0530

    initial commit

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the 1st numb: ");
+    scanf("%d",&a);
+     printf("Enter the 2nd numb: ");
+    scanf("%d",&b);
+    c=a+b;
+    printf("Sum of two nujm :%d\n");
+    return 0;
+
+}
